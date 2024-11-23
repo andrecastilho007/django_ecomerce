27af85c350b4bff2f1a93087112b85bede3f5f46
andrecastilho007 <andrecastilho007@hotmail.com>
2024-11-23 20:57:36 -0300

    formulario de contato

---
+++ e_commerce/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    nomecompleto = forms.CharField()
+    error_messages={'required': 'Obrigatório o preenchimento do nome'},
+    widget=forms.TextInput(
+            attrs={
+                    "class": "form-control", 
+                    "placeholder": "Seu nome completo"
+                }
+            )
+    email = forms.EmailField()
+    error_messages={'required': 'Obrigatório o preenchimento do nome'},
+    widget=forms.EmailInput(
+            attrs={
+                    "class": "form-control", 
+                    "placeholder": "Digite seu email"
+                }
+            )
+    mensagem = forms.CharField()
+    error_messages={'required': 'Obrigatório o preenchimento do nome'},
+    widget=forms.Textarea(
+            attrs={
+                    "class": "form-control", 
+                    "placeholder": "Digite sua mensagem"
+                }
+            )
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("O Email deve ser do gmail.com")
+        return email
